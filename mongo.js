69875341d2d6378d1ff96fd442f4994d5cf69aa2
Aaron Hull <aahull08@gmail.com>
2022-09-13 18:56:23 -0500

    Setup routes

--- mongo.js
+++ mongo.js
@@ -1,7 +1,12 @@
 // need to install mongo
 // just need to save and return id for pg to keep
 
+const express = require("express");
 const mongoose = require('mongoose');
+const Router = require("./routes")
+
+const app = express()
+app.use(express.json())
 
 main().catch(err => console.log(err));
 
@@ -9,13 +14,25 @@ async function main() {
   await mongoose.connect('mongodb://localhost:27017/test');
 }
 
-const requestSchema = new mongoose.Schema({
-  name: String
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", function () {
+  console.log("Connected successfully");
 });
 
-const Request = mongoose.model('Request', requestSchema);
+// const requestSchema = new mongoose.Schema({
+//   name: String
+// });
 
-const myRequest = new Request({ name: 'myRequest' });
-console.log(myRequest.name);
+// const Request = mongoose.model('Request', requestSchema);
 
-myRequest.save().then(result => console.log(result));
+// const myRequest = new Request({ name: 'myRequest' });
+// console.log(myRequest.name);
+
+// myRequest.save().then(result => console.log(result));
+
+app.use(Router);
+
+app.listen(3000, () => {
+  console.log("Server is running at port 3000");
+})
