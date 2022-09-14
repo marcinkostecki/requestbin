591f978755d74bd54a7ff522fdd340e6a18b727b
Eugene O'Fallon <70714033+epofallon@users.noreply.github.com>
2022-09-13 21:25:16 -0500

    refactor backend and dataService module

---
+++ dataService/mongo.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const reqModel = require('./models');
+
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://localhost:27017/requestBin');
+}
+
+async function insertOne(req) {
+  const reqDoc = new reqModel(req);
+  const { _id: id } = await reqDoc.save();
+  // const stringId = id.toString();
+  // const result = await reqModel.findById(stringId)
+  // console.log("Aaron wants to try: ", result)
+  return id.toString();
+}
+
+function readOne(id) {
+  return reqModel.findById(id);
+}
+
+
+module.exports = { insertOne, readOne };
